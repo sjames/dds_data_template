de51a53e6030470b1825828144451219e06002c1
Sojan James <sojan.james@gmail.com>
2020-09-27 20:52:10 +0530

    Feature flag to enable rust codegen

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,14 @@
 #![allow(non_snake_case)]
 
 use std::env;
-
+#[cfg(not(feature = "rust_codegen"))]
+use cyclonedds_sys::DDSGenType;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+// if rust_codegen is not enabled, the allocators are generated
+// in a different file
+#[cfg(not(feature = "rust_codegen"))]
+include!(concat!(env!("OUT_DIR"), "/DdsAllocator_impl.rs"));
 
 
 #[cfg(test)]
